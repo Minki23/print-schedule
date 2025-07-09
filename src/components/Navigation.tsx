103db0aeb639471c776c0891e37946233c5f48ed
Minki23 <michal.tomasz.szymczak@gmail.com>
2025-07-09 17:10:08 +0200

    Feat: responsive navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,9 +2,11 @@
 
 import Link from 'next/link';
 import { useSession, signOut } from 'next-auth/react';
+import { useState } from 'react';
 
 export default function Navigation() {
   const { data: session } = useSession();
+  const [showMenu, setShowMenu] = useState(false);
 
   if (!session) {
     return null;
@@ -26,6 +28,27 @@ export default function Navigation() {
           >
             Wyloguj się
           </button>
+          {window.innerWidth > 640 && (
+            <>
+              <Link href="/schedule" className="btn btn-primary btn-sm">Harmonogram</Link>
+              <Link href="/filaments" className="btn btn-primary btn-sm">Filamenty</Link>
+              {(session.user.rank === 'admin') && <Link href="/admin" className="btn btn-primary btn-sm">Panel Admina</Link>}
+            </>
+          )}
+          {window.innerWidth < 640 && (
+            <>
+            {showMenu && (
+                <div className="nav-dropdown">
+                  <Link href="/schedule" className="btn btn-primary btn-sm">Harmonogram</Link>
+                  <Link href="/filaments" className="btn btn-primary btn-sm">Filamenty</Link>
+                  {(session.user.rank === 'admin') && <Link href="/admin" className="btn btn-primary btn-sm">Panel Admina</Link>}
+                </div>
+              )}
+              <button onClick={() => setShowMenu(!showMenu)} className='nav-toggle'>
+                |||
+              </button>
+            </>
+          )}
         </div>
       </div>
     </nav>
